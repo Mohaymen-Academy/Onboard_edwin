591d049dcdcc81c3ba1471012856431f0132832f
edwinans <edyansari5@gmail.com>
2025-11-25 15:27:45 +0330

    todo

--- search/src/main/java/org/engine/Main.java
+++ search/src/main/java/org/engine/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Reader reader = new Reader();
         reader.readDocs("search/src/main/resources/SWBooks");
-
+        // TODO: use best practices to read under resources (resource stream?)
+        
         HashMap<String, String> docs = reader.getDocs();
         SearchEngine engine = new SearchEngine(docs);
 
