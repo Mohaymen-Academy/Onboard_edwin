47d96fcbe5090820c468df26b559959b4fd12982
edwinans <edyansari5@gmail.com>
2025-11-29 10:44:02 +0330

    mv AdvanceQueryParser outside

--- search/src/main/java/org/search/engine/impl/InvertedSearchEngine.java
+++ search/src/main/java/org/search/engine/impl/InvertedSearchEngine.java
@@ -8,40 +8,6 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-class AdvanceQueryParser {
-    private final HashSet<String> andPatterns, orPatterns, notPatterns;
-
-    public AdvanceQueryParser(String query) {
-        andPatterns = new HashSet<>();
-        orPatterns = new HashSet<>();
-        notPatterns = new HashSet<>();
-
-        Arrays.stream(query.split(" "))
-                .filter(Predicate.not(String::isBlank))
-                .forEach(pattern -> {
-                    String head = pattern.substring(0, 1);
-                    String tail = pattern.substring(1);
-                    switch (head) {
-                        case "+" -> orPatterns.add(tail);
-                        case "-" -> notPatterns.add(tail);
-                        default -> andPatterns.add(pattern);
-                    }
-                });
-    }
-
-    public HashSet<String> getAndPatterns() {
-        return andPatterns;
-    }
-
-    public HashSet<String> getOrPatterns() {
-        return orPatterns;
-    }
-
-    public HashSet<String> getNotPatterns() {
-        return notPatterns;
-    }
-}
-
 public class InvertedSearchEngine implements SearchEngine {
     /*
      * Implements inverted index algorithm
